71d453b8d3db85d78bf13c1555a4c2ef86e23c22
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-04-07 21:32:48 +0530

    production code

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import openai
+import os
 
 openai.api_key = os.environ.get('API_KEY')
 
---
+++ static/main.js
@@ -0,0 +1,30 @@
+
+function sendAPIRequest() {
+    const apiRequest = document.getElementById("api-request").value;
+    const responseDiv = document.getElementById("response-div");
+    responseDiv.innerHTML = 'Thinking of the best solution to your prompt... \
+    <br>Sip some &#9749; and wait for magic';
+    
+    const requestOptions = {
+        method: 'POST',
+        cache: "no-cache",
+        headers: { 'Content-Type':  'application/json' },
+        body: JSON.stringify({ 'api_request': apiRequest })
+    };
+    console.log(requestOptions);
+    fetch('http://127.0.0.1:8000/getInformation', requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            
+            responseDiv.innerHTML = data.code;
+            Prism.highlightAll();
+            console.log(data);
+        })
+        .catch(error => console.error(error));
+
+}
