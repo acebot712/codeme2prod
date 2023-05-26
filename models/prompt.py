5ae0c625821af2438c578237f724741588ba6cf5
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-05-27 02:39:38 +0530

    add prompt

---
+++ models/prompt.py
@@ -0,0 +1,81 @@
+class Prompt:
+    def __init__(self, text, *args, **kwargs):
+        self.im_start_token = kwargs.get("IM_START_TOKEN", "<|im_start|>")
+        self.im_end_token = kwargs.get("IM_END_TOKEN", "<|im_end|>")
+        self.run_prompt_engine = kwargs.get("run_prompt_engine", False)
+        self.text = self.prompt_engine(text) if self.run_prompt_engine else text
+
+    def __str__(self):
+        return self.text
+
+    def get_text(self):
+        return self.text
+
+    def set_text(self, text):
+        self.text = text
+
+    def completion_dic_to_chatml(self, completion_dic):
+        return (
+            self.im_start_token
+            + completion_dic.get("role", "user")
+            + "\n"
+            + completion_dic.get("content", "")
+            + "\n"
+            + self.im_end_token
+            + "\n"
+        )
+
+    def prompt_list_to_chatml_list(self, prompt_list):
+        for prompt_dic in prompt_list:
+            yield self.completion_dic_to_chatml(prompt_dic)
+
+    def prompt_engine(self, prompt):
+        prompt_list = [
+            {
+                "role": "system",
+                "content": "You are an AI programming assistant.",
+            },
+            {
+                "role": "system",
+                "content": "When asked for your name, you must respond with 'CodeMe'.",
+            },
+            {
+                "role": "system",
+                "content": "If the user asks for code or technical questions, you must provide code suggestions and adhere to technical information.",
+            },
+            {
+                "role": "system",
+                "content": "CodeMe MUST decline to answer is the question is not related to a developer.",
+            },
+            {
+                "role": "system",
+                "content": "First think step-by-step - describe your plan for what to build in pseudocode, written out in great detail.",
+            },
+            {
+                "role": "system",
+                "content": "Then output the code in a single code block.",
+            },
+            {
+                "role": "system",
+                "content": "Minimize other prose.",
+            },
+            {
+                "role": "system",
+                "content": "Use markdown formatting in your answers.",
+            },
+            {
+                "role": "system",
+                "content": "Make sure to include the programming language name at the start of the Markdown code blocks.",
+            },
+            {
+                "role": "system",
+                "content": "Avoid wrapping the whole response in triple backticks",
+            },
+            {
+                "role": "system",
+                "content": "Use markdown formatting in your answers.",
+            },
+        ]
+        prompt_list.append({"role": "user", "content": prompt})
+        prompt = "".join(self.prompt_list_to_chatml_list(prompt_list))
+        return prompt
