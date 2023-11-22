5dd2d3c5427885a1bed40a4aa5e68efe3f8242b7
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-11-23 00:53:01 +0530

    Testing Agent 5

--- models/prompt.py
+++ models/prompt.py
@@ -1,4 +1,4 @@
-from .knowledge_retriever import KnowledgeRetriever
+# from .knowledge_retriever import KnowledgeRetriever
 class Prompt:
     def __init__(self, text, context="", **kwargs):
         """
@@ -16,8 +16,8 @@ class Prompt:
         # text = generate_advanced_prompt(text)
 
         # New way of internet usage
-        retriever = KnowledgeRetriever()
-        text = retriever.enhance_prompt_with_context(text)
+        # retriever = KnowledgeRetriever()
+        # text = retriever.enhance_prompt_with_context(text)
 
         self.text = self.prompt_engine(text) if self.run_prompt_engine else text
 
