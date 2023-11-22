8f46937472a45ca17d87072ed17d37cb9c34887e
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-11-22 22:58:53 +0530

    Testing internet faulty 7

--- api.py
+++ api.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from models.completion import Completion
 from models.turbo import AzureChatGPTAPI
 from models.prompt import Prompt
 import os
 from dotenv import load_dotenv
-import pandas as pd
+# import pandas as pd
 import datetime
+from fastapi.responses import JSONResponse
+
 
 load_dotenv()
 
@@ -24,52 +26,52 @@ app.add_middleware(
 
 @app.post("/getcode/{model}")
 async def get_information(req: Request, model: str):
-    req_body = await req.json()
-    session_cookie = req.headers.get("Cookie")
-    if session_cookie:
-        session_cookie = session_cookie.split("; ")[0].split("=")[1]
-    # print("session_cookie =", session_cookie)
+    try:
+        req_body = await req.json()
+        session_cookie = req.headers.get("Cookie")
+        if session_cookie:
+            session_cookie = session_cookie.split("; ")[0].split("=")[1]
+        # print("session_cookie =", session_cookie)
 
-    if model == "turbo":
-        IM_START_TOKEN = "<|im_start|>"
-        IM_END_TOKEN = "<|im_end|>"
-        prompt_object = Prompt(
-            text=req_body["prompt"],
-            context=req_body["context"],
-            im_start_token=IM_START_TOKEN,
-            im_end_token=IM_END_TOKEN,
-            run_prompt_engine=True,
-        )
-        model = AzureChatGPTAPI(
-            api_key=os.environ.get("FK_API_KEY", ""),
-            endpoint=os.environ.get("FK_ENDPOINT", ""),
-            im_start_token=IM_START_TOKEN,
-            im_end_token=IM_END_TOKEN,
-        )
+        if model == "turbo":
+            IM_START_TOKEN = "<|im_start|>"
+            IM_END_TOKEN = "<|im_end|>"
+            prompt_object = Prompt(
+                text=req_body["prompt"],
+                context=req_body["context"],
+                im_start_token=IM_START_TOKEN,
+                im_end_token=IM_END_TOKEN,
+                run_prompt_engine=True,
+            )
+            model = AzureChatGPTAPI(
+                api_key=os.environ.get("FK_API_KEY", ""),
+                endpoint=os.environ.get("FK_ENDPOINT", ""),
+                im_start_token=IM_START_TOKEN,
+                im_end_token=IM_END_TOKEN,
+            )
+            
+        elif model == "completions":
+
+            model = Completion(api_key=os.environ.get('OPENAI_API_KEY'))
+            prompt_object = Prompt(
+                text=req_body["prompt"],
+                context=req_body["context"]
+            )
+            print("prompt_object =", prompt_object)
+            print("prompt_object.get_text() =", prompt_object.get_text())
+
+        else:
+            raise HTTPException(status_code=404, detail="Model not found")
         
-    elif model == "completions":
-
-        model = Completion(api_key=os.environ.get('OPENAI_API_KEY'))
-        prompt_object = Prompt(
-            text=req_body["prompt"],
-            context=req_body["context"]
-        )
-        print("prompt_object =", prompt_object)
-        print("prompt_object.get_text() =", prompt_object.get_text())
-
-    else:
-        return {
-            "status" : "FAILURE",
-            "code" : "Model not found"
+        code_data = model.generate_code(prompt_object)
+        response = {
+            "status" : "SUCCESS",
+            "code" : code_data
         }
+        return JSONResponse(status_code=200, content=response)
     
-    code_data = model.generate_code(prompt_object)
-    print(code_data)
-    response = {
-        "status" : "SUCCESS",
-        "code" : code_data
-    }
-    print(response)
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"status": "FAILURE", "error": str(e)})
 
     # Create a DataFrame from the two lists
     # data = req_body
@@ -88,8 +90,6 @@ async def get_information(req: Request, model: str):
     # else:
     #     # Append to existing csv file
     #     df.to_csv(date_string, mode='a', header=False, index=False)
-
-    return response
     """
     curl -X POST -H "Content-Type: application/json" -d '{"prompt": "Write a function for dfs"}' -b "session_cookie=cookie_monster" http://localhost:8000/getcode/turbo
     curl -X POST -H "Content-Type: application/json" -d '{"prompt": "Code for DFS function", "context": ""}' -b "session_cookie=cookie_monster" http://35.223.253.138:8000/getcode/turbo
